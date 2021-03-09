77b7373aa4d14662b101579f0230792e286eef81
William Pettersson <william@ewpettersson.se>
2021-03-09 16:19:57 +0000

    Update R script
    Now gives example using R data, meaning the file "should" also run as a
    decent test.

--- optimise_graph.R
+++ optimise_graph.R
@@ -1,7 +1,7 @@
 library(reticulate)
 
 # W_filename and data_filename are actual names to files.
-optimise_graph <- function(W_filename, data_filename) {
+optimise_graph_from_file <- function(W_filename, data_filename) {
   utils <- import_from_path("utils")
   optimiser <- import_from_path("optimiser")
   data <- utils$read_data(data_filename)
@@ -14,3 +14,28 @@ optimise_graph <- function(W_filename, data_filename) {
 # some usual graph functions, the one that's probably relevant is graph$edges()
 # which gives a list of the edges still ini the graph.
 # > optimal_graph$edges()
+
+
+# This function uses data from R, rather than reading from files.
+# W_matrix needs to be either a list of lists of zeros and ones,
+# or it needs to be a matrix of zeros and ones.
+# data_list needs to be a list of numbers.
+optimise_graph_from_data <- function(W_matrix, data_list) {
+  if (all(class(W_matrix) == c("matrix", "array"))) {
+    W_matrix = lapply(apply(W_matrix, 1, as.list), unlist) 
+  }
+  optimiser <- import_from_path("optimiser")
+  opt_object <- optimiser$Optimiser(W_matrix, data_list)
+  optimal_graph <- opt_object$iterative_opt(remove=TRUE, add=FALSE)
+  optimal_graph
+}
+
+W <- list(list(0,1,0,0,0,0),
+          list(1,0,1,0,0,0),
+          list(0,1,0,1,0,0),
+          list(0,0,1,0,1,0),
+          list(0,0,0,1,0,1),
+          list(0,0,0,0,1,0))
+
+data <- list(100, 99, 0, 100, 199, 200)
+optimise_graph_from_data(W, data)
